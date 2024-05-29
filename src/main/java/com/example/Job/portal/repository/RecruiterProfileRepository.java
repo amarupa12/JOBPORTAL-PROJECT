6f1896e925274adc999db9839f491e8c7aa98255
amarupa12 <114008648+amarupa12@users.noreply.github.com>
2024-05-29 23:00:30 +0530

    Delete src directory

--- src/main/java/com/example/Job/portal/repository/RecruiterProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Job.portal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Job.portal.entity.RecruiterProfile;
-
-public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
-}
